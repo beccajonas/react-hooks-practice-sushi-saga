6c24a897ed8de454db00c6331445065847205ef6
Becca Jonas <beccajonas3@gmail.com>
2023-11-26 10:22:41 -0500

    code set up

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState, useEffect }from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
+
 function App() {
+
+  useEffect( () => {
+    fetch(API)
+    .then(res => res.json())
+    .then(data => console.log(data))
+  }, [])
+
   return (
     <div className="app">
       <SushiContainer />
